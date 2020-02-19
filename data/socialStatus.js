50a1874a69211c9ed3f76fec2b690fe17bf86c8b
Ade Adeola <adedotster@gmail.com>
2020-02-19 12:14:24 +0100

    Update social status generator

--- data/socialStatus.js
+++ data/socialStatus.js
@@ -1,5 +1,3 @@
-
-
 const socialStatus = {
   main: [
     "Underclass", //8==> 8
@@ -7,37 +5,27 @@ const socialStatus = {
     "Working class", //40==> 8
     "Lower middle class", // 26==> 13
     "Upper middle class", //13==> 40
-    "Upper class", //8==> 26
+    "Upper class" //8==> 26
   ],
-  odds : {
-    "Preschool": [
-      40, 26, 8, 6, 8, 13
-    ],
-    "Primary/Elementary school": [
-      40, 26, 8, 6, 8, 13
-    ],
-    "Junior secondary/Middle school": [
-      26, 40, 8, 8, 6, 6
-    ],
-    "Senior secondary/High school": [
-      26, 40, 8, 8, 6, 6
-    ],
-    "Diploma/Associate degrees": [
-      6, 26, 40, 8, 8, 13
-    ],
-    Degree: [
-      6, 8, 13, 40, 26, 8
-    ],
-    Masters: [
-      8, 6, 8, 13, 40, 26
-    ],
-    Doctorate: [
-      8, 6, 8, 13, 40, 26
-    ],
-    others: [ //regardless of education
-      8, 6, 40, 26, 13, 8
+  odds: {
+    Preschool: [47.72, 34.13, 13.59, 2.14, 2.14, 1],
+    "Primary/Elementary school": [13.59, 47.72, 34.13, 2.14, 2.14, 1],
+    "Junior secondary/Middle school": [2.14, 13.59, 68.26, 13.59, 2.14, 1],
+    "Senior secondary/High school": [2.14, 13.59, 47.72, 34.13, 2.14, 1],
+    "Diploma/Associate degrees": [2.14, 2.14, 13.59, 68.26, 13.59, 1],
+    Degree: [2.14, 2.14, 34.13, 47.72, 13.59, 1],
+    Masters: [2.14, 2.14, 13.59, 47.72, 34.13, 1],
+    Doctorate: [2.14, 2.14, 13.59, 34.13, 47.72, 1],
+    others: [
+      //regardless of education
+      2.14,
+      13.59,
+      47.72,
+      34.13,
+      2.14,
+      1
     ]
   }
-}
+};
 
 export default socialStatus;
